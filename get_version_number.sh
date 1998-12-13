24b179b9065d5df13aa910a42c7ed1a50f27abd5
Burchard Steinbild <bs@suse.de>
1998-12-13 23:06:23 +0100

    - delete usr/i486-linux-libc6/lib/libqimgio.so if it is a symlink - sbin/init.d/network:  do not delete network aliases in stop. - updated runlevel.fallback - removed man pages for fstab and nfs (were forgotten in util). - fixed /etc/SuSE-release

--- get_version_number.sh
+++ get_version_number.sh
@@ -1,1 +1,4 @@
 get_date_version_string `find_newest_file *.*`
+
+echo "BUILD_DISTRIBUTION_NAME=\"$BUILD_DISTRIBUTION_NAME\"" > aaa_base_versions
+echo "BUILD_DISTRIBUTION_VERSION=\"$BUILD_DISTRIBUTION_VERSION\"" >> aaa_base_versions
