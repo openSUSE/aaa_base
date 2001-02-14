26b9793dd5d225aeda3cf0b13d509999cc81f745
Werner Fink <werner@suse.de>
2001-02-14 18:10:11 +0100

    - Migrate showconsole to sysvinit

--- showconsole-0.9/libblogger.h
+++
@@ -1,6 +0,0 @@
-extern int bootlog(const int lvl, const char *fmt, ...);
-#define B_NOTICE	((int)'n')	/* Notice  */
-#define B_DONE		((int)'d')	/* Done    */
-#define B_FAILED	((int)'f')	/* Failed  */
-#define B_SKIPPED	((int)'s')	/* Skipped */
-#define B_UNUSED	((int)'u')	/* Unused  */
--- showconsole-0.9/libconsole.h
+++
@@ -1,6 +0,0 @@
-extern void pushd(const char * path);
-extern void popd(void);
-extern char * fetchtty(const pid_t pid, const pid_t ppid);
-extern void prepareIO(void (*rfunc)(int), void (*cfunc)(void), const int in, const int out);
-extern void safeIO (void);
-extern void closeIO(void);
