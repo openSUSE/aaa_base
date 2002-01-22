526edd7d8cbdb6e8aedd34a9b7a715a948fdb393
Rüdiger Oertel <ro@suse.de>
2002-01-22 14:50:29 +0100

    - CREATE_JAVA_LINK is in sysconfig/java again - fixed SuSEconfig.doublecheck

---
+++ rc.config_parts/sysconfig.java
@@ -0,0 +1,9 @@
+#
+# SuSEconfig can automatically create the links /usr/lib/java and /usr/lib/jre
+# that point to a suitable JDK or JRE respectively if you set CREATE_JAVALINK
+# to "yes"
+# If you are not satisfied with the choice it makes, set CREATE_JAVALINK to
+# "no" and set the link manually.
+#
+CREATE_JAVALINK="yes"
+
