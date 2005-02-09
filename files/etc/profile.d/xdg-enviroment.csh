e69355f557b44ad4a2e0d6420adc13218a1af87c
Rüdiger Oertel <ro@suse.de>
2005-02-09 15:03:28 +0100

    - moved /etc/profile.d/xdg-enviroment.* from desktop-data-SuSE   to this package

---
+++ files/etc/profile.d/xdg-enviroment.csh
@@ -0,0 +1,3 @@
+if ( ${?XDG_DATA_DIRS} ) then
+    setenv XDG_DATA_DIRS /usr/share/:/usr/local/share/:/etc/opt/kde3/share/:/opt/kde3/share/:/opt/gnome/share/
+endif
---
+++ files/etc/profile.d/xdg-enviroment.sh
@@ -0,0 +1,3 @@
+if test -z "$XDG_DATA_DIRS" ; then
+    export XDG_DATA_DIRS=/usr/share/:/usr/local/share/:/etc/opt/kde3/share/:/opt/kde3/share/:/opt/gnome/share/
+fi
