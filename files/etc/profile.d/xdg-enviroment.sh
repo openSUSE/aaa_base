80866d16afed8250acae36f22d58424a0af68592
Rüdiger Oertel <ro@suse.de>
2005-02-23 14:54:22 +0100

    - updated etc/profile.d/xdg*

--- files/etc/profile.d/xdg-enviroment.sh
+++ files/etc/profile.d/xdg-enviroment.sh
@@ -1,3 +1,6 @@
 if test -z "$XDG_DATA_DIRS" ; then
-    export XDG_DATA_DIRS=/usr/share/:/usr/local/share/:/etc/opt/kde3/share/:/opt/kde3/share/:/opt/gnome/share/
+    export XDG_DATA_DIRS=/usr/local/share/:/usr/share/:/etc/opt/kde3/share/:/opt/kde3/share/:/opt/gnome/share/
+fi
+if test -z "$XDG_CONFIG_DIRS" ; then
+    export XDG_CONFIG_DIRS=/usr/local/etc/xdg/:/etc/xdg/:/etc/opt/gnome/xdg/
 fi
