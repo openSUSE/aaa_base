37efb8685b6e2d5957ab7f928758aadc63db0be6
Marcus Meissner <meissner@suse.de>
2006-11-07 12:58:35 +0100

    - disable MALLOC debugging for the 10.2 release.

--- files/etc/profile.d/malloc-debug.sh
+++ files/etc/profile.d/malloc-debug.sh
@@ -3,6 +3,6 @@
 # - meissner@suse.de
 #
 # fill new malloc areas with 0x42
-export MALLOC_PERTURB_=B
+#export MALLOC_PERTURB_=B
 # Abort on any malloc related error.
-export MALLOC_CHECK_=2
+#export MALLOC_CHECK_=2
