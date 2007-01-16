8d596a9b9087fa44a47b7d10d627362ecd12a1c0
Marcus Meissner <meissner@suse.de>
2007-01-16 12:01:38 +0100

    - enable malloc debug again for factory.

--- files/etc/profile.d/malloc-debug.sh
+++ files/etc/profile.d/malloc-debug.sh
@@ -2,7 +2,7 @@
 # This is for FACTORY only and should be removed for the release.
 # - meissner@suse.de
 #
-# fill new malloc areas with 0x42
-#export MALLOC_PERTURB_=B
+# fill new malloc areas with 0x43
+export MALLOC_PERTURB_=C
 # Abort on any malloc related error.
-#export MALLOC_CHECK_=2
+export MALLOC_CHECK_=2
