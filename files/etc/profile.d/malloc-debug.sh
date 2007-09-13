a1b9bd95ce54371dde472490b95cd64b0966fb62
Marcus Meissner <meissner@suse.de>
2007-09-13 13:59:58 +0100

    - remove MALLOC_CHECK_ for RC1.

--- files/etc/profile.d/malloc-debug.sh
+++
@@ -1,8 +0,0 @@
-# 
-# This is for FACTORY only and should be removed for the release.
-# - meissner@suse.de
-#
-# fill new malloc areas with 0x43
-export MALLOC_PERTURB_=C
-# Abort on any malloc related error.
-export MALLOC_CHECK_=2
