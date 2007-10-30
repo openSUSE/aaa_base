f63970963e191d1a5fa2dc4799bf9aecff1ef3c8
Rüdiger Oertel <ro@suse.de>
2007-10-30 19:44:16 +0100

    revision 1365
    Tue Oct 30 18:44:16 CET 2007 - ro@suse.de
    
    - added sysconf_addword to /usr/sbin (#328599)
    
    -------------------------------------------------------------------
    Tue Oct 30 18:39:37 CET 2007 - ro@suse.de
    
    - fix setting for XDG_DATA_DIRS (#300678)
    
    -------------------------------------------------------------------
    Tue Oct 30 18:32:39 CET 2007 - ro@suse.de
    
    - remove remounting of rootfs from boot.rootfsck
      (handled by initrd for quite a while already)
      (#335174,#286759)
    
    -------------------------------------------------------------------

--- files/etc/profile.d/xdg-environment.sh
+++ files/etc/profile.d/xdg-environment.sh
@@ -1,3 +1,26 @@
+# uniquefy_search_path (search_path):
+#
+# Remove duplicate entries from a search path string, preserving order.
+uniquefy_search_path ()
+{
+  OIFS="$IFS"
+  IFS='
+'
+  set -- $(echo ${1+"$@"} | sed -r 's@/*:|([^\\]):@\1\n@g;H;x;s@/\n@\n@')
+  IFS="$OIFS"
+
+  _y=""
+  for _x ; do
+    case ":${_y}:" in
+      *:"${_x}":*) continue
+    esac
+    _y=${_y:+"$_y:"}${_x}
+  done
+
+  echo "${_y}"
+  unset _y _x
+}
+
 for xdgdir in /usr/local/share /usr/share /etc/opt/*/share /opt/*/share /usr/share/gnome ; do
    if test -d $xdgdir && test -d $xdgdir/applications; then
       if test -z "$XDG_DATA_DIRS"; then
@@ -7,6 +30,8 @@ for xdgdir in /usr/local/share /usr/share /etc/opt/*/share /opt/*/share /usr/sha
       fi
    fi
 done
+
+XDG_DATA_DIRS=$(uniquefy_search_path "$XDG_DATA_DIRS")
 export XDG_DATA_DIRS
 
 for xdgdir in /usr/local/etc/xdg /etc/xdg /etc/opt/*/xdg ; do
@@ -18,6 +43,8 @@ for xdgdir in /usr/local/etc/xdg /etc/xdg /etc/opt/*/xdg ; do
       fi
    fi
 done
+
+XDG_CONFIG_DIRS=$(uniquefy_search_path "$XDG_CONFIG_DIRS")
 export XDG_CONFIG_DIRS
 
 unset xdgdir
