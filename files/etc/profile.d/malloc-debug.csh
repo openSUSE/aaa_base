a1e35749b7b1f83baf787e222a1f57b6ee628ada
Various <nobody@suse.de>
2009-02-19 12:19:02 +0100

    revision 1451
    Thu Feb 19 11:19:02 CET 2009 - schwab@suse.de
    
    - Add .xz to /etc/DIR_COLORS.
    
    -------------------------------------------------------------------
    Thu Feb 19 10:49:54 CET 2009 - meissner@suse.de
    
    - reenable MALLOC_CHECK_ and MALLOC_PERTURB_ for Factory.
    
    -------------------------------------------------------------------

---
+++ files/etc/profile.d/malloc-debug.csh
@@ -0,0 +1,3 @@
+# malloc debugging helpers (see man malloc), remove again for RC1 - meissner@suse.de
+setenv MALLOC_CHECK_ 2
+setenv MALLOC_PERTURB_ E
---
+++ files/etc/profile.d/malloc-debug.sh
@@ -0,0 +1,3 @@
+# malloc debugging helpers (see man malloc), remove again for RC1 - meissner@suse.de
+export MALLOC_CHECK_=2
+export MALLOC_PERTURB_=E
