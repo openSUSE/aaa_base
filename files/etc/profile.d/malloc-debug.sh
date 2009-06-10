03fb8fd647dd70d41b1c69bbb4a4449af4c73d86
Rüdiger Oertel <ro@suse.de>
2009-06-10 15:34:26 +0100

    - change condition for MALLOC_CHECK to test for /.buildenv

--- files/etc/profile.d/malloc-debug.sh
+++ files/etc/profile.d/malloc-debug.sh
@@ -1,5 +1,5 @@
 # malloc debugging helpers (see man malloc), remove again for RC1 - meissner@suse.de
-if test ! -d /.build.packages; then
+if test ! -d /.buildenv ; then
   # disable for now in build environments - too many unknown problems (bnc#509398)
   export MALLOC_CHECK_=3
 fi
