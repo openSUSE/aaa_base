ac69884b9c24844aa328ba9e44a8064f2db159b2
Stephan Kulow <coolo@novell.com>
2009-06-11 22:05:59 +0100

    - /.buildenv is almost never a directory...

--- files/etc/profile.d/malloc-debug.sh
+++ files/etc/profile.d/malloc-debug.sh
@@ -1,5 +1,5 @@
 # malloc debugging helpers (see man malloc), remove again for RC1 - meissner@suse.de
-if test ! -d /.buildenv ; then
+if test ! -f /.buildenv ; then
   # disable for now in build environments - too many unknown problems (bnc#509398)
   export MALLOC_CHECK_=3
 fi
