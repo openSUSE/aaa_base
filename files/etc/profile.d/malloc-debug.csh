35307c4f735d2ec56448e94038eaad12d3bc88a4
Stephan Kulow <coolo@suse.de>
2009-06-13 11:02:46 +0100

    - now that fixed glibc is in, we can enable malloc_check again

--- files/etc/profile.d/malloc-debug.csh
+++ files/etc/profile.d/malloc-debug.csh
@@ -1,4 +1,3 @@
 # malloc debugging helpers (see man malloc), remove again for RC1 - meissner@suse.de
-# disable for now in build environments - too many unknown problems (bnc#509398)
-#setenv MALLOC_CHECK_ 3
+setenv MALLOC_CHECK_ 3
 setenv MALLOC_PERTURB_ E
--- files/etc/profile.d/malloc-debug.sh
+++ files/etc/profile.d/malloc-debug.sh
@@ -1,6 +1,3 @@
 # malloc debugging helpers (see man malloc), remove again for RC1 - meissner@suse.de
-if test ! -f /.buildenv ; then
-  # disable for now in build environments - too many unknown problems (bnc#509398)
-  export MALLOC_CHECK_=3
-fi
+export MALLOC_CHECK_=3
 export MALLOC_PERTURB_=E
