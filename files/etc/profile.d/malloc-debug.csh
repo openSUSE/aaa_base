c604a782b536b23783ed847d2164ad531ceeba39
Marcus Meissner <meissner@suse.de>
2009-10-26 14:24:08 +0100

    - disable malloc debugging again for the release. (rm /etc/profile.d/malloc-debug.*)

--- files/etc/profile.d/malloc-debug.csh
+++
@@ -1,3 +0,0 @@
-# malloc debugging helpers (see man malloc), remove again for RC1 - meissner@suse.de
-setenv MALLOC_CHECK_ 3
-setenv MALLOC_PERTURB_ E
--- files/etc/profile.d/malloc-debug.sh
+++
@@ -1,3 +0,0 @@
-# malloc debugging helpers (see man malloc), remove again for RC1 - meissner@suse.de
-export MALLOC_CHECK_=3
-export MALLOC_PERTURB_=E
