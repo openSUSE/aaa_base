526f8d80465f8294c4146d59dbbe2c5efa90cebe
Various <nobody@suse.de>
2010-02-23 18:25:10 +0100

    revision 1497
    Tue Feb 23 17:25:10 CET 2010 - ro@suse.de
    
    - No longer call zic (the timezone compiler) from boot.clock for
      creating /etc/localtime: this file is created by YaST during
      installation; there is no need to recreate it at boot time.
      (This code was only executed on s390 and in xen guests, too.)
    
    -------------------------------------------------------------------
    Fri Feb 19 17:07:18 CET 2010 - meissner@suse.de
    
    - enabled MALLOC_CHECK_ and MALLOC_PERTURB_ during testing
    
    -------------------------------------------------------------------
    Thu Feb 18 17:41:50 CET 2010 - ro@suse.de
    
    - make sure options for /dev/pts are honored (bnc#580924)
    
    -------------------------------------------------------------------

---
+++ files/etc/profile.d/malloc-debug.csh
@@ -0,0 +1,3 @@
+# remove this file before shipment
+setenv MALLOC_CHECK_ 3
+setenv MALLOC_PERTURB_ 69
---
+++ files/etc/profile.d/malloc-debug.sh
@@ -0,0 +1,3 @@
+# remove this file before shipment
+export MALLOC_CHECK_=3
+export MALLOC_PERTURB_=69
