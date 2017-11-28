ef3a62492dcf1fc58f7a6691d3ae601e4118ce50
Ruediger Oertel <ro@suse.de>
2017-11-28 15:21:07 +0100

    move wsl specific code from profile into profile.d/wsl.sh

--- files/etc/profile.d/wsl.sh
+++ files/etc/profile.d/wsl.sh
@@ -1,6 +1,15 @@
 # WSL does not utilitze this pam functionality currently.
-if test `umask` = 0000; then
-    UMASK_LOGIN_DEFS=`sed -ne 's/^UMASK[[:space:]]*//p' /etc/login.defs`
-    test "$UMASK_LOGIN_DEFS" && umask "$UMASK_LOGIN_DEFS"
-    unset UMASK_LOGIN_DEFS
+if test -f /proc/version ; then
+    IS_WSL=$(grep -i microsoft /proc/version)
+fi
+
+if test -n "$IS_WSL" ; then
+    if test -n "$ORIG_PATH" ; then
+	PATH=$ORIG_PATH:$PATH
+    fi
+    if test $(umask) = 0000; then
+	UMASK_LOGIN_DEFS=$(sed -ne 's/^UMASK[[:space:]]*//p' /etc/login.defs)
+	test "$UMASK_LOGIN_DEFS" && umask "$UMASK_LOGIN_DEFS"
+	unset UMASK_LOGIN_DEFS
+    fi
 fi
