04deec6baa2184d0ff62009340ac4f689d2ec886
Werner Fink <werner@suse.de>
2021-11-24 13:02:28 +0100

    Avoid changing COLORTERM variable in urxvt
    Signed-off-by: Werner Fink <werner@suse.de>

--- files/usr/etc/profile.d/term.sh
+++ files/usr/etc/profile.d/term.sh
@@ -1,7 +1,9 @@
 #
 # Midnight Commander needs this to run in color mode
 #
-if test "$(tput colors 2>/dev/null || echo 0)" -gt 8; then
-    COLORTERM=truecolor
-    export COLORTERM
+if test -n "${TERM}" -a "${TERM%-*}" != rxvt -a -z "${COLORTERM}"; then
+    if test "$(tput colors 2>/dev/null || echo 0)" -gt 8; then
+	: ${COLORTERM=truecolor}
+	export COLORTERM
+    fi
 fi
