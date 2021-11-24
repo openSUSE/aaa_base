5486aadee23a19422cc54064dc6c95ee47f87cf8
Werner Fink <werner@suse.de>
2021-11-24 15:09:43 +0100

    Remove term.sh and term.csh: no COLORTERM anymore
    as described by Adam Majer the mc does not need them
    anymore
    
    Signed-off-by: Werner Fink <werner@suse.de>

--- files/usr/etc/profile.d/term.csh
+++
@@ -1,8 +0,0 @@
-#
-# Midnight Commander needs this to run in color mode
-#
-if ( ${?TERM} && ! ${?COLORTERM} ) then
-    if ($TERM !~ rxvt.* && "`tput colors || echo 0`" > 8) then
-	setenv COLORTERM truecolor
-    endif
-endif
--- files/usr/etc/profile.d/term.sh
+++
@@ -1,9 +0,0 @@
-#
-# Midnight Commander needs this to run in color mode
-#
-if test -n "${TERM}" -a "${TERM%-*}" != rxvt -a -z "${COLORTERM}"; then
-    if test "$(tput colors 2>/dev/null || echo 0)" -gt 8; then
-	: ${COLORTERM=truecolor}
-	export COLORTERM
-    fi
-fi
