a6ed3bad7396e60789e50715e70aacfad73dbf37
Rüdiger Oertel <ro@suse.de>
2024-02-02 13:37:32 +0100

    properly shorten the variable when setting JAVA_HOME and JRE_HOME

--- files/usr/etc/profile.d/alljava.sh
+++ files/usr/etc/profile.d/alljava.sh
@@ -11,13 +11,13 @@
 
 if test -L /etc/alternatives/java -a -e /etc/alternatives/java; then
     ALTERNATIVES_JAVA_LINK=`realpath /etc/alternatives/java 2> /dev/null`
-    export JRE_HOME=${ALTERNATIVES_JAVA_LINK/\/bin\/java}
+    export JRE_HOME=${ALTERNATIVES_JAVA_LINK%/bin/java}
     unset ALTERNATIVES_JAVA_LINK
 fi
 
 if test -L /etc/alternatives/javac -a -e /etc/alternatives/javac; then
     ALTERNATIVES_JAVAC_LINK=`realpath /etc/alternatives/javac 2> /dev/null`
-    export JAVA_HOME=${ALTERNATIVES_JAVAC_LINK/\/bin\/javac}
+    export JAVA_HOME=${ALTERNATIVES_JAVAC_LINK%/bin/javac}
     export JAVA_BINDIR=$JAVA_HOME/bin
     export JDK_HOME=$JAVA_HOME
     export SDK_HOME=$JAVA_HOME
