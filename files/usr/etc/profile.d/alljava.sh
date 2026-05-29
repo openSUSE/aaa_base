b5c4f9964b79186d5cb3315891b0033b1d2c27ae
Fridrich Štrba <fridrich.strba@bluewin.ch>
2026-05-29 14:46:13 +0200

    Simplifying the sh part too

--- files/usr/etc/profile.d/alljava.sh
+++ files/usr/etc/profile.d/alljava.sh
@@ -11,34 +11,35 @@
 
 if command -v alts >/dev/null 2>&1; then
     if alts -t java >/dev/null 2>&1; then
-        JRE_HOME=$(alts -t java)
-        export JRE_HOME=${JRE_HOME%/bin/java}
+        JAVA_TARGET=$(alts -t java)
     fi
 
     if alts -t javac >/dev/null 2>&1; then
-        JAVA_HOME=$(alts -t javac)
-        export JAVA_HOME=${JAVA_HOME%/bin/javac}
+        JAVAC_TARGET=$(alts -t javac)
     fi
 fi
 
-if [ -z "$JRE_HOME" ]; then
+if [ -z "$JAVA_TARGET" ]; then
     if test -L /etc/alternatives/java -a -e /etc/alternatives/java; then
-        ALTERNATIVES_JAVA_LINK=$(realpath /etc/alternatives/java 2>/dev/null)
-        export JRE_HOME=${ALTERNATIVES_JAVA_LINK%/bin/java}
-        unset ALTERNATIVES_JAVA_LINK
+        JAVA_TARGET=$(realpath /etc/alternatives/java 2>/dev/null)
     fi
 fi
 
-if [ -z "$JAVA_HOME" ]; then
+if [ -z "$JAVAC_TARGET" ]; then
     if test -L /etc/alternatives/javac -a -e /etc/alternatives/javac; then
-        ALTERNATIVES_JAVAC_LINK=$(realpath /etc/alternatives/javac 2>/dev/null)
-        export JAVA_HOME=${ALTERNATIVES_JAVAC_LINK%/bin/javac}
-        unset ALTERNATIVES_JAVAC_LINK
+        JAVAC_TARGET=$(realpath /etc/alternatives/javac 2>/dev/null)
     fi
 fi
 
-if [ ! -z "$JAVA_HOME" ]; then
-    export JAVA_BINDIR="$JAVA_HOME/bin"
+if [ ! -z "$JAVA_TARGET" ]; then
+    export JRE_HOME=${JRE_HOME%/bin/java}
+    unset JAVA_TARGET
+fi
+
+if [ ! -z "$JAVAC_TARGET" ]; then
+    export JAVA_HOME=${JAVAC_TARGET%/bin/javac}
+    export JAVA_BINDIR=${JAVAC_TARGET%/javac}
     export JDK_HOME="$JAVA_HOME"
     export SDK_HOME="$JAVA_HOME"
+    unset JAVAC_TARGET
 fi
